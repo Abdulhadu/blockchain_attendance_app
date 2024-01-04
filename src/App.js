ea4e1df668d98a79238acbea2334606fc29dda89
Abdul hadi <hadi27767@gmail.com>
2024-01-04 22:32:10 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <h1>Welcome to React App of BlockChain Attendance System</h1>
+    </div>
+  );
+}
+
+export default App;
